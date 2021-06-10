83ec16939175db1a87b5a781600a95a378ee3eff
mwhite13 <mwhite13@ggc.edu>
2021-06-10 02:57:19 -0400

    navbar test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Navbar, { Navigation } from './components/Navbar/Navbar.jsx';
+
+
+function App() {
+  return (
+    
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {AppBar, Container, Toolbar, Typography} from '@material-ui/core'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
